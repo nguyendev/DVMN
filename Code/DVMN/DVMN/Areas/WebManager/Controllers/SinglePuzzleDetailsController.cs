36fe7e0323cf077f16c1d5ec707a6a0d05f549e6
nguyendev <nguyen.nah76@gmail.com>
2017-07-12 11:21:24 +0700

    SubmitMultiForm

--- Code/DVMN/DVMN/Areas/WebManager/Controllers/SinglePuzzleDetailsController.cs
+++ Code/DVMN/DVMN/Areas/WebManager/Controllers/SinglePuzzleDetailsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DVMN.Services;
 using DVMN.Models;
+using Newtonsoft.Json;
 
 namespace DVMN.Areas.WebManager.Controllers
 {
@@ -15,26 +16,58 @@ namespace DVMN.Areas.WebManager.Controllers
         {
             return View();
         }
-        [HttpGet]
         public IActionResult Create()
         {
             var multiPuzzle =  HttpContext.Session.GetObjectFromJson<MultiPuzzle>("MultiPuzzle");
+            var listSinglePuzzleDetails = HttpContext.Session.GetObjectFromJson <List<Temp>>("listSinglePuzzleDetails");
+            if (listSinglePuzzleDetails == null)
+            {
+               
+                List<Temp> list = new List<Temp>(multiPuzzle.NumberQuestion - 1);
+                for (int i = 0; i <= multiPuzzle.NumberQuestion - 1; i++)
+                {
+                    list.Add(new Temp { ID = i });
+                }
+                ViewData["listSinglePuzzleDetails"] = list.ToList();
+                HttpContext.Session.SetObjectAsJson("listSinglePuzzleDetails", list);
+            }
+            else
+            {
+                ViewData["listSinglePuzzleDetails"] = listSinglePuzzleDetails.ToList();
+            }
             ViewData["NumberQuestion"] = multiPuzzle.NumberQuestion;
             return View();
         }
         [HttpPost]
-
-        public string Create(Temp temp)
+        [Route("sendData")]
+        public IActionResult SaveTemp(Temp temp,string id)
         {
-            
-                //HttpContext.Session.SetObjectAsJson("cau" + id, model);
-            return "1";
-            
-            //var MultiPuzzle= HttpContext.Session.GetObjectFromJson<MultiPuzzle>("MultiPuzzle");
-            //MultiPuzzle.NumberQuestion;
-            //HttpContext.Session.SetObjectAsJson("")
+            var oldListSinglePuzzleDetails = HttpContext.Session.GetObjectFromJson<List<Temp>>("listSinglePuzzleDetails");
+            var multiPuzzle = HttpContext.Session.GetObjectFromJson<MultiPuzzle>("MultiPuzzle");
 
-            return "0";
+            //luu gia tri moi vao session
+            List<Temp> newListSinglePuzzleDetails = new List<Temp>(multiPuzzle.NumberQuestion-1);
+            foreach (var item in oldListSinglePuzzleDetails)
+            {
+                if (item.ID == Int32.Parse(id))
+                {
+                    item.Title = temp.Title;
+                    item.Description = temp.Description;
+                    item.Image = temp.Image;
+                    item.AnswerA = temp.AnswerA;
+                    item.AnswerB = temp.AnswerB;
+                    item.AnswerC = temp.AnswerC;
+                    item.AnswerD = temp.AnswerD;
+                    item.Correct = temp.Correct;
+                    item.IsYesNo = temp.IsYesNo;
+                }
+                newListSinglePuzzleDetails.Add(item);
+            }
+            HttpContext.Session.SetObjectAsJson("listSinglePuzzleDetails", newListSinglePuzzleDetails);
+            ViewData["listSinglePuzzleDetails"] = newListSinglePuzzleDetails.ToList();
+
+            var serializedJsonModel = JsonConvert.SerializeObject(newListSinglePuzzleDetails.ToList());
+            return Json(serializedJsonModel);
         }
 
         //public IActionResult SavePuzzleToSession([Bind("Title", "Description", "Image", "IsYesNo", "AnswerA", "AnswerB", "AnswerC", "AnswerD", "Correct")] SinglePuzzleDetails model)
--- Code/DVMN/DVMN/Models/Temp.cs
+++ Code/DVMN/DVMN/Models/Temp.cs
@@ -7,8 +7,15 @@ namespace DVMN.Models
 {
     public class Temp
     {
-        public string Title { get; set; }
-
         public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string IsYesNo { get; set; }
+        public string AnswerA { get; set; }
+        public string AnswerB { get; set; }
+        public string AnswerC { get; set; }
+        public string AnswerD { get; set; }
+        public int Correct { get; set; }
     }
 }
